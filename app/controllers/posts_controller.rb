895be575b36ff037c73cac5edf63d6549f12ea92
jihaneh <jijih@hotmail.fr>
2022-05-03 21:44:03 +0200

    Add create post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,10 +4,28 @@ class PostsController < ApplicationController
   end
 
   def new
-    
+    @post = Post.new
+    @user = User.find(1)
+  end
+
+  def create
+    Post.create(post_params)
+    redirect_to { user_posts(current_user) }
   end
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    p = params.require(:post).permit(:title, :text)
+    p[:author] = current_user
+    p
+  end
+
+  def post_params_setup
+    Post.create(author: current_user, title: params[:post][:title], text: params[:post][:text])
   end
 end
