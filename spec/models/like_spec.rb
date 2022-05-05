b31d7ee0f19c733112521d9274c123ad038bb3d4
jihaneh <jijih@hotmail.fr>
2022-05-05 10:21:15 +0200

    Add unit spec

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:all) do
+    @user = User.create(name: 'Emy', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from France.')
+    @post = Post.create(title: 'my post', author: @user)
+  end
+
+  it 'should have increments the like_counter s post after saving the post' do
+    expect(@post.likes_counter).to be 0
+
+    like = Like.new(author: @user, post: @post)
+    like.save
+    expect(@post.likes_counter).to be 1
+  end
 end
