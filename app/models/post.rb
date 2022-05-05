a8a7802ea2826c911e39706a101a25e63e92a3da
jihaneh <jijih@hotmail.fr>
2022-05-05 08:17:27 +0200

    Add Post validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,10 @@ class Post < ApplicationRecord
   has_many :comments
   after_save :update_counter
 
+  validates :title, presence: true, allow_blank: false, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
+
   def update_counter
     author.increment!(:posts_counter)
   end
