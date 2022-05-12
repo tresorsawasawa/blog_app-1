7e49d6a6cf518783fb263505f037e5d7dff9a755
jihaneh <jijih@hotmail.fr>
2022-05-12 23:30:00 +0200

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { registrations: 'users/registrations' }
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users, :controllers => { registrations: 'users/registrations' }
 
   root "users#index"
   resources :users, only: [:index, :show] do
@@ -9,6 +8,15 @@ Rails.application.routes.draw do
       resources :comments, only: [:new, :create, :destroy]
       resources :likes, only: [:new, :create]
     end
+  end
+
+  namespace :api do
+    get 'authentication' , to: 'authentication#new'
+    post 'authentication/create' , to: 'authentication#create'
+    get 'users/:id/posts', to: 'apiposts#index'
+    get 'users/:user_id/posts/:id/comments', to: 'apicomments#index', as: 'comments'
+    get 'users/:user_id/posts/:id/comments/new', to: 'apicomments#new'
+    post 'users/:user_id/posts/:id/comments/create', to: 'apicomments#create', as: 'create_comment'
   end
 
 end
